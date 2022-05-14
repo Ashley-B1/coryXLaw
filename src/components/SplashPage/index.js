0166130b99d8f73a8bf7883340e9e45451e7b089
Ashley (Skye) Brown <stormi.seuneu@outlook.com>
2022-05-13 23:58:56 -0500

    added some styling to splash page

--- src/components/SplashPage/index.js
+++ src/components/SplashPage/index.js
@@ -3,7 +3,13 @@ import './SplashPage.css';
 
 const SplashPage = () => {
   return (
-    <div className='splash-page'>SplashPage</div>
+    <div className='splash-page'>
+      <h1 className='main-title'>CoryXLaw</h1>
+      <h3 className='slogan'>Shogun At Law</h3>
+      <div className='splash-info'>
+        a div with stuff in it
+      </div>
+    </div>
   )
 }
 
