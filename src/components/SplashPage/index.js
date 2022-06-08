73b05dfd7d73371e2986e3ea9aca5b8680d153b7
Ashley (Skye) Brown <stormi.seuneu@outlook.com>
2022-06-08 15:07:41 -0500

    added revised header

--- src/components/SplashPage/index.js
+++ src/components/SplashPage/index.js
@@ -4,8 +4,11 @@ import './SplashPage.css';
 const SplashPage = () => {
   return (
     <div className='splash-page'>
-      <h1 className='main-title'>CoryXLaw</h1>
-      <h3 className='slogan'>Shogun At Law</h3>
+      <div className='header'>
+        <i class="main-icon fa-solid fa-spa"></i>
+        <h1 className='main-title'>CORY X. KENSHIN</h1>
+        <h3 className='slogan'>Shogun At Law</h3>
+      </div>
       <div className='splash-info'>
         just adding something to my garden!!!
       </div>
